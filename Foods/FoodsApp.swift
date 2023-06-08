1ad339bf0d62c8b1eaa8231c68e5bd78a978c9a8
Deepika <deepikardb@gmail.com>
2023-06-07 17:19:00 -0700

    Initial Commit

---
+++ Foods/FoodsApp.swift
@@ -0,0 +1,19 @@
+//
+//  FoodsApp.swift
+//  Foods
+//
+//  Created by Deepika Ramesh on 6/7/23.
+//
+
+
+
+import SwiftUI
+
+@main
+struct FoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
